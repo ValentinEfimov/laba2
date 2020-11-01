fed87e8afaa76af96f8eff552be244a6ecb77729
ValentinEfimov <valentinefimov00@gmail.com>
2020-11-02 00:02:06 +0300

    added formula for calculating function 1 and 2

--- src/bsu/rfe/java/group7/lab2/Efimov/varB5/MainFrame.java
+++ src/bsu/rfe/java/group7/lab2/Efimov/varB5/MainFrame.java
@@ -1,6 +1,29 @@
 package bsu.rfe.java.group7.lab2.Efimov.varB5;
+import javax.swing.*;
+import java.lang.*;
 
-public class MainFrame {
+public class MainFrame extends JFrame {
     private static final int WIDTH = 400;
     private static final int HEIGHT = 320;
+    private JTextField textFieldX;
+    private JTextField textFieldY;
+// Текстовое поле для отображения результата,
+
+    // как компонент, совместно используемый в различных методах
+    private JTextField textFieldResult;
+    // Группа радио-кнопок для обеспечения уникальности выделения в группе
+    private ButtonGroup radioButtons = new ButtonGroup();
+    // Контейнер для отображения радио-кнопок
+    private Box hboxFormulaType = Box.createHorizontalBox();
+    // Идентификатор выбранной формулы
+    private int formulaId = 1;
+    public Double calculate1(Double x, Double y, Double z) {
+        return Math.pow(Math.cos(Math.PI*Math.pow(x,3))+Math.log(Math.pow(1+y,2)),1/4)*(Math.exp(z*z)+Math.sqrt(1/x)+Math.cos(Math.exp(y)));
+    }
+    // Формула №2 для рассчѐта
+    public Double calculate2(Double x, Double y, Double z) {
+        return Math.exp(0.5*x)/(Math.sqrt(z+y)*Math.log(Math.pow(x,z)));
+    }
+
+
 }
