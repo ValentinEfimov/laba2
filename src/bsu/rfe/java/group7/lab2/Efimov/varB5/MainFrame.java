9aa825bbe658e4d7bf2065a0bb06a41e0f96274b
ValentinEfimov <valentinefimov00@gmail.com>
2020-11-02 15:50:51 +0300

    added helper method for creating Radio Buttons

--- src/bsu/rfe/java/group7/lab2/Efimov/varB5/MainFrame.java
+++ src/bsu/rfe/java/group7/lab2/Efimov/varB5/MainFrame.java
@@ -1,6 +1,25 @@
 package bsu.rfe.java.group7.lab2.Efimov.varB5;
 import javax.swing.*;
 import java.lang.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
 
 public class MainFrame extends JFrame {
     private static final int WIDTH = 400;
@@ -17,13 +36,27 @@ public class MainFrame extends JFrame {
     private Box hboxFormulaType = Box.createHorizontalBox();
     // Идентификатор выбранной формулы
     private int formulaId = 1;
+
     public Double calculate1(Double x, Double y, Double z) {
-        return Math.pow(Math.cos(Math.PI*Math.pow(x,3))+Math.log(Math.pow(1+y,2)),1/4)*(Math.exp(z*z)+Math.sqrt(1/x)+Math.cos(Math.exp(y)));
+        return Math.pow(Math.cos(Math.PI * Math.pow(x, 3)) + Math.log(Math.pow(1 + y, 2)), 1 / 4) * (Math.exp(z * z) + Math.sqrt(1 / x) + Math.cos(Math.exp(y)));
     }
+
     // Формула №2 для рассчѐта
     public Double calculate2(Double x, Double y, Double z) {
-        return Math.exp(0.5*x)/(Math.sqrt(z+y)*Math.log(Math.pow(x,z)));
+        return Math.exp(0.5 * x) / (Math.sqrt(z + y) * Math.log(Math.pow(x, z)));
     }
 
+    // Вспомогательный метод для добавления кнопок на панель
+    private void addRadioButton(String buttonName, final int formulaId) {
+        JRadioButton button = new JRadioButton(buttonName);
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {
+                MainFrame.this.formulaId = formulaId;
+            }
+        });
+        radioButtons.add(button);
+        hboxFormulaType.add(button);
 
+
+    }
 }
